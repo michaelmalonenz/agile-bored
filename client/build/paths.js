50bd8f4eae231ce6b87b871bbad3570a960e7800
Michael Malone <michael.j.b.malone@gmail.com>
2017-07-30 20:26:07 +1200

    copy the glyphicons packaged with bootstrap to the output dir

--- client/build/paths.js
+++ client/build/paths.js
@@ -1,16 +1,17 @@
-var appRoot = 'src/';
-var outputRoot = 'dist/';
-var exportSrvRoot = '/export/';
+var appRoot = 'src/'
+var outputRoot = 'dist/'
+var exportSrvRoot = '/export/'
 
 module.exports = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
   scss: appRoot + '**/*.scss',
+  fonts: 'node_modules/bootstrap-sass/assets/fonts/bootstrap/*',
   style: appRoot + 'styles/**/*.css',
   output: outputRoot,
   exportSrv: exportSrvRoot,
   doc: './doc',
   e2eSpecsSrc: 'test/e2e/src/**/*.js',
   e2eSpecsDist: 'test/e2e/dist/'
-};
+}
