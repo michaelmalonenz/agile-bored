51e9d42cf724f04f4caa175f8aad05a7c3d18fd1
Michael Malone <michael.j.b.malone@gmail.com>
2017-09-05 22:11:44 +1200

    Lay the columns out nicely
    The columns all have the same height, though that isn't
    necessarily set to a minimum of the viewport - topShelf.height.
    
    The issue cards within the columns take their max width from the
    columns themselves, which is handy, as the columns have their width
    set programmatically by percentage.

--- client/src/status-column.js
+++ client/src/status-column.js
@@ -9,5 +9,11 @@ export class StatusColumn {
   constructor () {
   }
 
+  get statusIssues () {
+    return this.issues.filter(x => {
+      return (x.status == null && this.status.name === 'Todo') ||
+        (x.status != null && x.status.name === this.status.name)
+    })
+  }
   
 }
