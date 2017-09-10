cbf30128ebfe1007a96c1ac8da86d1ee0e467401
Michael Malone <michael.j.b.malone@gmail.com>
2017-09-10 18:30:11 +1200

    Update to babel-6

--- client/build/babel-options.js
+++ client/build/babel-options.js
@@ -1,11 +1,26 @@
-module.exports = {
-  modules: 'system',
-  moduleIds: false,
-  comments: false,
-  compact: false,
-  stage: 2,
-  optional: [
-    'es7.decorators',
-    'es7.classProperties'
-  ]
-};
+var path = require('path')
+
+exports.base = function (modules) {
+  return {
+    filename: '',
+    filenameRelative: '',
+    sourceMaps: true,
+    sourceRoot: '',
+    moduleRoot: path.resolve('src').replace(/\\/g, '/'),
+    moduleIds: false,
+    comments: false,
+    compact: false,
+    code: true,
+    presets: [ ['es2015', { loose: true, modules: modules }], 'stage-1' ],
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-runtime'
+    ]
+  }
+}
+
+exports.system = function () {
+  var options = exports.base()
+  options.plugins.push('transform-es2015-modules-systemjs')
+  return options
+}
