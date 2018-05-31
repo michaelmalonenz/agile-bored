90ea538682d0c02947c64a889a155e76ed0f01b0
Michael Malone <michael.j.b.malone@gmail.com>
2018-05-31 19:06:01 +1200

    refresh the board on drop

--- client/src/issue.js
+++ client/src/issue.js
@@ -1,4 +1,4 @@
-import {ISSUE_DELETED} from './events'
+import {ISSUE_DELETED, REFRESH_BOARD} from './events'
 import {IssueEditorDialog} from './dialogs/issue-editor'
 
 export class Issue {
@@ -95,6 +95,7 @@ export class Issue {
   updateStatus (status) {
     this.issueService.updateStatus(this.issue.id, status.id).then(() => {
       this.issue.IssueStatus = status
+      this.eventAggregator.publish(REFRESH_BOARD)
     })
   }
 }
