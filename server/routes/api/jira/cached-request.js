4c677632b9a085933951a821982845bc3d82a414
Michael Malone <michael.j.b.malone@gmail.com>
2018-06-25 14:40:23 +1200

    Changed the memory leak into a cache
    Another name for a cache without an invalidation policy is a memory
    leak.  I added an invalidation policy, even if we don't clean it up,
    it's still better than nothing.

--- server/routes/api/jira/cached-request.js
+++ server/routes/api/jira/cached-request.js
@@ -1,5 +1,6 @@
 const request = require('request-promise-native')
 
+const ONE_DAY = 1000 * 60 * 60 * 24
 const cache = {}
 
 class CachedResponse {
@@ -7,10 +8,14 @@ class CachedResponse {
     this.content = content
     this.cachedAt = Date.now()
   }
+
+  get cacheTimeValid () {
+    return (Date.now() - this.cachedAt) < ONE_DAY
+  }
 }
 
 module.exports = function (options) {
-  if (cache[options.uri]) {
+  if (cache[options.uri] && cache[options.uri].cacheTimeValid) {
     return Promise.resolve(cache[options.uri].content)
   } else {
     return request(options).then(result => {
