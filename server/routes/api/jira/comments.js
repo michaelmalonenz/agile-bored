52194e4507404ffd5852c96e49ebfd936093c989
Michael Malone <michael.j.b.malone@gmail.com>
2018-07-09 19:18:22 +1200

    That's everything, right?

--- server/routes/api/jira/comments.js
+++ server/routes/api/jira/comments.js
@@ -4,8 +4,8 @@ const CommentViewModel = require('../../../viewmodels/comment')
 
 module.exports = {
   getCommentsForIssue: function (req, res) {
-    return jiraRequestBuilder.jira(`/issue/${req.params.issueId}/comment`, req)
-      .then(options => request(options))
+    const options = jiraRequestBuilder.jira(`/issue/${req.params.issueId}/comment`, req)
+    return request(options)
       .then(result => {
         const results = []
         for (let comment of result.comments) {
@@ -16,11 +16,9 @@ module.exports = {
       .catch(err => res.status(502).send(JSON.stringify(err)))
   },
   addComment: function (req, res) {
-    return jiraRequestBuilder.jira(`/issue/${req.params.issueId}/comment`, req, 'POST')
-      .then(options => {
-        options.body = req.body
-        return request(options)
-      })
+    const options = jiraRequestBuilder.jira(`/issue/${req.params.issueId}/comment`, req, 'POST')
+    options.body = req.body
+    return request(options)
       .then(result => res.send(CommentViewModel.createFromJira(result)))
       .catch(err => res.status(502).send(err))
   }
