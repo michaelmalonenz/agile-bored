ca0eef43fff869cdcf3a2c6e8ee56fa7a362f57f
Michael Malone <michael.malone@aranzgeo.com>
2018-08-12 18:13:51 +1200

    Make the parent issue editable from the board

--- client/src/issues/parent-issue.js
+++ client/src/issues/parent-issue.js
@@ -1,8 +1,31 @@
-import {customElement, bindable} from 'aurelia-framework'
+import {bindable,customElement,inject} from 'aurelia-framework'
+import { DialogService } from 'aurelia-dialog';
+import { IssueService } from '../services/issues';
+import {IssueEditorDialog} from '../dialogs/issue-editor'
+import {IssueViewModelFactory} from '../factories/issue-viewmodel-factory'
 
 @customElement('parent-issue')
 @bindable('parent')
 @bindable('statuses')
+@inject(DialogService, IssueService)
 export class ParentIssue {
+  constructor(dialogService, issueService) {
+    this.dialogService = dialogService
+    this.issueService = issueService
+  }
 
+  editIssue () {
+    this.dialogService.open({ viewModel: IssueEditorDialog,
+      lock: true,
+      keyboard: ['Escape'],
+      model: {
+        issue: this.parent.issue,
+        edit: true
+      }
+    }).whenClosed(async response => {
+      if (!response.wasCancelled) {
+        this.parent.issue = await this.issueService.update(response.output)
+      }
+    })
+  }
 }
