144fce947ced1d06cc6bb13677355d8f7a7c98fe
Michael Malone <michael.j.b.malone@gmail.com>
2018-09-29 17:20:48 +1200

    Made the Epic headings sticky

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,16 @@
 import environment from './environment'
+import { PLATFORM } from 'aurelia-pal'
 import { UserService } from './services/users'
 import { SecuritySettings } from './security/security-settings'
+import 'bootstrap'
 
 export async function configure (aurelia) {
   aurelia.use
     .standardConfiguration()
-    .plugin('aurelia-dialog')
+    .plugin(PLATFORM.moduleName('aurelia-dialog'), config => {
+      config.useDefaults()
+      config.settings.startingZIndex = 1050
+    })
     .plugin('aurelia-dragula')
     .feature('resources')
     .feature('widgets')
