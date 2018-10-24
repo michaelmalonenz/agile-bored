70013ec8db51eae06225d3a8f8b8ab5d0077ace2
Michael Malone <michael.j.b.malone@gmail.com>
2018-10-25 07:41:04 +1300

    Small improvements to subtasks list

--- client/src/widgets/sub-task-list.js
+++ client/src/widgets/sub-task-list.js
@@ -3,5 +3,12 @@ import {bindable,customElement} from 'aurelia-framework'
 @bindable('issues')
 @customElement('sub-task-list')
 export class SubTaskList {
+    constructor () {
+        this.diplay = null
+    }
+
+    displayTask (issue) {
+        this.display = issue
+    }
 
 }
