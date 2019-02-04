43e989615c06b887ebc277b3f7742c1adc2f7f8b
Michael Malone <michael.j.b.malone@gmail.com>
2019-02-04 14:06:14 +1300

    Allow updating status from edit screen
    Allow sending the issue back to the backlog and from the backlog to the
    board without losing context.

--- client/src/widgets/issue-description.js
+++ client/src/widgets/issue-description.js
@@ -1,11 +1,24 @@
-import { customElement, bindable, bindingMode } from 'aurelia-framework'
+import { customElement, bindable, bindingMode, inject } from 'aurelia-framework'
 
-@bindable('issue')
+import { IssueService } from '../services/issues'
+
+@inject(IssueService)
+@bindable({
+  name: 'issue',
+  defaultBindingMode: bindingMode.twoWay
+})
 @bindable({
   name: 'edit',
   defaultBindingMode: bindingMode.twoWay
 })
 @customElement('issue-description')
 export class IssueDescription {
+  constructor (issueService) {
+    this.issueService = issueService
+  }
 
+  async updateStatus (status) {
+    await this.issueService.updateStatus(this.issue.id, status.id)
+    this.issue = await this.issueService.get(this.issue.id)
+  }
 }
