f2f3671175af2e9549d9ca72627d3738d7857b82
Michael Malone <michael.malone@aranzgeo.com>
2019-03-08 18:11:11 +1300

    Fixed up the Create issue trigger

--- server/migrations/20190308012845-add-history-triggers.js
+++ server/migrations/20190308012845-add-history-triggers.js
@@ -22,20 +22,20 @@ module.exports = {
         IF NEW."typeId" <> OLD."typeId" THEN
           INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
           VALUES (NEW.id, 'type', NEW."latestEditorId",
-            (SELECT name FROM issue_type WHERE id = OLD.typeId),
-            (SELECT name FROM issue_type WHERE id = NEW.typeId), current_timestamp);
+            (SELECT name FROM issue_type WHERE id = OLD."typeId"),
+            (SELECT name FROM issue_type WHERE id = NEW."typeId"), current_timestamp);
         END IF;
         IF NEW."assigneeId" <> OLD."assigneeId" THEN
           INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
           VALUES (NEW.id, 'assignee', NEW."latestEditorId",
-            (SELECT displayName FROM users WHERE id = OLD."assigneeId"),
-            (SELECT displayName FROM users WHERE id = NEW."assigneeId"), current_timestamp);
+            (SELECT "displayName" FROM users WHERE id = OLD."assigneeId"),
+            (SELECT "displayName" FROM users WHERE id = NEW."assigneeId"), current_timestamp);
         END IF;
         IF NEW."reporterId" <> OLD."reporterId" THEN
           INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
           VALUES (NEW.id, 'reporter', NEW."latestEditorId",
-            (SELECT displayName FROM users WHERE id = OLD."reporterId"),
-            (SELECT displayName FROM users WHERE id = NEW."reporterId"), current_timestamp);
+            (SELECT "displayName" FROM users WHERE id = OLD."reporterId"),
+            (SELECT "displayName" FROM users WHERE id = NEW."reporterId"), current_timestamp);
         END IF;
         IF NEW."parentId" <> OLD."parentId" THEN
         INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
@@ -45,9 +45,49 @@ module.exports = {
         RETURN NEW;
       END; $$ LANGUAGE plpgsql;`
     ).then(() => {
+      queryInterface.sequelize.query(`
+      CREATE OR REPLACE FUNCTION changelogIssueInsert() RETURNS TRIGGER AS $$
+      BEGIN
+        IF NEW."title" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'title', NEW."latestEditorId", '', NEW.title, current_timestamp);
+        END IF;
+        IF NEW."description" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'description', NEW."latestEditorId", '', NEW."description", current_timestamp);
+        END IF;
+        IF NEW."statusId" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'status', NEW."latestEditorId", NULL,
+            (SELECT name FROM issue_statuses WHERE id = NEW."statusId"), current_timestamp);
+        END IF;
+        IF NEW."typeId" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'type', NEW."latestEditorId", NULL,
+            (SELECT name FROM issue_type WHERE id = NEW."typeId"), current_timestamp);
+        END IF;
+        IF NEW."assigneeId" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'assignee', NEW."latestEditorId", NULL,
+            (SELECT "displayName" FROM users WHERE id = NEW."assigneeId"), current_timestamp);
+        END IF;
+        IF NEW."reporterId" IS NOT NULL THEN
+          INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'reporter', NEW."latestEditorId", NULL,
+            (SELECT "displayName" FROM users WHERE id = NEW."reporterId"), current_timestamp);
+        END IF;
+        IF NEW."parentId" IS NOT NULL THEN
+        INSERT INTO changelog ("issueId", "field", "authorId", "oldValue", "newValue", "timestamp")
+          VALUES (NEW.id, 'parent', NEW."latestEditorId", NULL, CAST(NEW."parentId" AS TEXT), current_timestamp);
+        END IF;
+
+        RETURN NEW;
+      END; $$ LANGUAGE plpgsql;`
+    )
+    }).then(() => {
       return queryInterface.sequelize.query(`
   CREATE TRIGGER issue_insert_changelog_trigger AFTER INSERT
-  ON issues FOR EACH ROW EXECUTE PROCEDURE changelogIssueDiff();`)
+  ON issues FOR EACH ROW EXECUTE PROCEDURE changelogIssueInsert();`)
     }).then(() => {
       return queryInterface.sequelize.query(`
   CREATE TRIGGER issue_update_changelog_trigger AFTER UPDATE
@@ -62,6 +102,9 @@ module.exports = {
     })
     .then(() => {
       return queryInterface.sequelize.query('DROP FUNCTION changelogIssueDiff();')
+    })
+    .then(() => {
+      return queryInterface.sequelize.query('DROP FUNCTION changelogIssueInsert();')
     })
   }
 }
