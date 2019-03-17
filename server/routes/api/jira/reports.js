6a0bf00484dfec90ad71f68f5b2985cbcd7670c3
Michael Malone <michael.malone@aranzgeo.com>
2019-03-17 21:11:31 +1300

    Started working on Epic burndown thingy

--- server/routes/api/jira/reports.js
+++ server/routes/api/jira/reports.js
@@ -1,5 +1,7 @@
 const request = require('request-promise-native')
 const jiraRequestBuilder = require('./jira-request')
+const ChangeLogViewModel = require('../../../viewmodels/changelog')
+const moment = require('moment')
 
 const maxResults = 150
 
@@ -22,6 +24,33 @@ module.exports = {
       results = results.concat(res.issues)
     }
     res.send(collateResults(results))
+  },
+  epicRemaining (req, res) {
+    const options = jiraRequestBuilder.agile(`/epic/${req.query.epicId}/issue?expand=changelog`, req)
+    return request(options)
+      .then(result => {
+        for (let issue of result.issues) {
+          const events = []
+          for (let history of issue.changelog.histories) {
+            events.push(...ChangeLogViewModel.createFromJira(history))
+          }
+          const statusEvents = events.filter(e => e.field === 'status')
+          statusEvents.sort((a, b) => a.timestamp - b.timestamp)
+        }
+        // const start = moment(statusEvents[0].timestamp)
+        const start = result.fields.created
+        const now = moment()
+        const data = {}
+        for (let current = start; current.isBefore(now); current.add(1, 'day')) {
+          const currentDatum = {
+            resolved: 0,
+            inProgress: 0,
+            toDo: 0
+          }
+          data[current.format('YYYY-MM-DD')] = currentDatum
+        }
+        res.send(data)
+      })
   }
 }
 
