0c3192fe03fcb4c456623499a34e50f1053336f2
Michael Malone <michael.j.b.malone@gmail.com>
2019-04-22 17:14:44 +1200

    Add more appropriate icons for the reports sub-routes

--- client/src/reports.js
+++ client/src/reports.js
@@ -3,8 +3,8 @@ export class Reports {
     this.router = router
     config.map([
       { route: '', redirect: 'burnup' },
-      { route: 'burnup', name: 'burnup', moduleId: 'reports/burnup', nav: true, title: 'Burn Up', auth: true, settings: { icon: 'fa-th-list' } },
-      { route: 'westrum', name: 'westrum', moduleId: 'reports/westrum', nav: true, title: 'Westrum', auth: true, settings: { icon: 'fa-search' } }
+      { route: 'burnup', name: 'burnup', moduleId: 'reports/burnup', nav: true, title: 'Burn Up', auth: true, settings: { icon: 'fa-fire-alt' } },
+      { route: 'westrum', name: 'westrum', moduleId: 'reports/westrum', nav: true, title: 'Westrum', auth: true, settings: { icon: 'fa-ruler' } }
     ])
   }
 }
