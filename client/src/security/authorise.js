90ca64d5fe947ab36c49db8bc7ff1e6a36208e19
Michael Malone <michael.j.b.malone@gmail.com>
2020-09-22 18:06:22 +1200

    Minor JS changes + bottom margin on board

--- client/src/security/authorise.js
+++ client/src/security/authorise.js
@@ -7,7 +7,7 @@ export class AuthorizeStep {
     if (instruction.getAllInstructions().some(i => i.config.auth)) {
       var isLoggedIn = SecuritySettings.instance().loggedIn
       if (!isLoggedIn) {
-        const currentUrl = instruction.fragment + (instruction.queryString ? `?${instruction.queryString}` : '');
+        const currentUrl = instruction.fragment + (instruction.queryString ? `?${instruction.queryString}` : '')
         return next.cancel(new RedirectToRoute('login', { return: currentUrl }))
       }
     }
